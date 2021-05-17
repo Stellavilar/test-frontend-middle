95de9327a8b8dba9aea9f43e32772082ad7ca7fd
Stella Vilar <vilar.stella@gmail.com>
2021-05-17 11:58:28 +0200

    Making of Pagination thanks to Material ui

--- src/components/List.js
+++ src/components/List.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Products from './Products';
+import Pagination from '@material-ui/lab/Pagination';
 import axios from 'axios';
 
 export default function List() {
@@ -7,16 +8,22 @@ export default function List() {
     //Get beers from API
     const [ beers, setBeers ] = useState([]);
 
+    //Controlled pagination
+    const [page, setPage] = useState(1);
+    const handleChange = (event, value) => {
+      setPage(value);
+    };
+
     //Fetch all beers
     useEffect(() => {
-        axios.get('beers')
+        axios.get(`beers?page=${page}&per_page=9`)
             .then((res) => {
                 setBeers(res.data)
             })
             .catch((err) => {
                 console.log(err)
             })
-    }, []);
+    }, [page]);
 
     return (
         <div className="container">
@@ -24,6 +31,7 @@ export default function List() {
                 { beers.map((beer, index) => 
                     <Products key={beer.id} beer={beer} index={index} />
                 )}
+                <Pagination count={9} onChange={handleChange} page={page} />
             </div>
         </div>
     );
