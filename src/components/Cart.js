176315809444db06e7dd440ccbd534aa5dc7d604
Stella Vilar <vilar.stella@gmail.com>
2021-05-17 13:52:34 +0200

    Display cart contents on Cart component

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from 'react';
+import {Card, CardContent, CardMedia, CardActions, Button} from '@material-ui/core';
+import { useCart } from '../context/CartContext';
+
+function CartItem ({ beer }) {
+    return (
+        <article>
+            <Card >
+                <CardMedia
+                    image={beer.image_url}
+                    title="Beer image"
+                />
+                <CardContent>
+                    <h2> {beer.name} </h2>
+                </CardContent>
+                <CardActions>
+                    <Button size="small" color="primary">
+                        See details
+                    </Button>
+                </CardActions>
+            </Card>
+        </article>
+    );
+}
 
 export default function Cart() {
+    //Get cart array stored into localStorage
+    const {cartContents} = useCart();
+
+    if(cartContents.length === 0) { 
+        return (
+            <div className="container">
+            <div className="content">
+                Your cart is empty
+            </div>
+        </div>
+        );
+    };
+
     return (
-        <div>
-            Cart
+        <div className="container">
+            <div className="content">
+                { cartContents.map((item, index) => (
+                    <CartItem
+                        key={index}
+                        beer={item}
+                        index={index}
+                    />
+                ))}
+            </div>
         </div>
     );
 };
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,9 +1,5 @@
 import React , { useState, useEffect } from 'react';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardActions from '@material-ui/core/CardActions';
-import Button from '@material-ui/core/Button';
+import {Card, CardContent, CardMedia, CardActions, Button} from '@material-ui/core';
 import {useCart }from '../context/CartContext';
 
 export default function Products({ beer }) {
@@ -49,7 +45,7 @@ export default function Products({ beer }) {
                 </CardContent>
                 <CardActions>
                     <Button size="small" color="primary">
-                        See more
+                        See details
                     </Button>
                 </CardActions>
                 { added ? 
