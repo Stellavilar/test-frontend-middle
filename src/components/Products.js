fb76e45cfd83115e172f105cf0004e7d9fe3b301
Stella Vilar <vilar.stella@gmail.com>
2021-05-17 12:20:34 +0200

    Management of button and card style when click on 'add to cart' button

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React , { useState } from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
@@ -6,13 +6,27 @@ import CardActions from '@material-ui/core/CardActions';
 import Button from '@material-ui/core/Button';
 
 export default function Products({ beer }) {
+    //State for onClick button management : if product is added to cart, display remove button and so on
+    const [ added, setAdded ] = useState(false);
+
+    function addToCart() {
+        setAdded(false);
+    };
+
+    function removeFromCart() {
+        setAdded(true);
+    };
+
     return (
         <article>
-            <Card>
+            <Card style={ added ? { backgroundColor: '#edeae8'} : { backgroundColor: '#ffffff'}}>
                 <CardMedia
                     image={beer.image_url}
                     title="Beer image"
                 />
+                { added ? <p className="card-badge" style={{color: 'green', textAlign: 'center'}}>
+                    Added to cart!
+                </p> : null}
                 <CardContent>
                     <h2> {beer.name} </h2>
                     <h3> Vol: {beer.volume.value} {beer.volume.unit} </h3>
@@ -22,7 +36,10 @@ export default function Products({ beer }) {
                         See more
                     </Button>
                 </CardActions>
-                <Button variant="contained" color="primary">ADD TO CART</Button>
+                { added ? 
+                <Button variant="contained" color="secondary" onClick={addToCart}>REMOVE FROM CART</Button> :
+                <Button variant="contained" color="primary" onClick={removeFromCart}>ADD TO CART</Button>
+                }
             </Card>
         </article>
     );
