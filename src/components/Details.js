13189a4e0a89b6f379832a55aa65f79d79faf723
Stella Vilar <vilar.stella@gmail.com>
2021-05-17 15:17:48 +0200

    We can see on details page if the item has been added to the cart or not

--- src/components/Details.js
+++ src/components/Details.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useParams} from 'react-router';
 import {Card, CardContent} from '@material-ui/core';
 import axios from 'axios';
+import {useCart} from '../context/CartContext';
 
 
 export default function Details() {
@@ -9,6 +10,10 @@ export default function Details() {
    const [ beer, setBeer ] = useState({});
    //Get id on the url params
    let {id} = useParams();
+   //Get cart array:
+   const { cartContents } = useCart();
+   //Management of the card background color to say that the card has been added to cart:
+    const [ added, setAdded ] = useState(false);
 
    useEffect(() => {
      //Get beers by id
@@ -19,14 +24,33 @@ export default function Details() {
      .catch((err) => {
          console.log(err)
      });
-   },[id]);
+     //Check if this item has been added to cart: 
+    //1. If the cart is filled check into it if there is an id similar to the id of the current item:
+    if(cartContents) {
+        // eslint-disable-next-line eqeqeq
+        if(cartContents.find( el => el.id == id )) {
+            //If there is the same id, so this card has been added to card
+            return setAdded(true) 
+        } else {
+            //If the id is not present into the cart, the card has not been added to the cart
+            return setAdded(false) 
+        }
+    }
+
+
+   },[id, cartContents]);
 
     return (
         <div className="container">
             <div className="content">
                 <article>
-                    <Card style={{width: '70%', height:'90%'}}>
+                    <Card  style={ added ? { backgroundColor: '#edeae8', width: '70%', height:'90%'} : { backgroundColor: '#ffffff', width: '70%', height:'90%'}}>
                         <img style={{width: '100px'}} src={beer.image_url} alt="beer" />
+                        { added ? 
+                        <p className="card-badge" style={{color: 'green', textAlign: 'center'}}>
+                            Added to cart!
+                        </p> 
+                        : null}
                         <CardContent>
                             <h2 style={{textAlign: 'center', fontSize:'1.8em'}}> {beer.name} </h2>
                             <h4>Ingredients:</h4>
@@ -44,10 +68,9 @@ export default function Details() {
                                     ) : null}
                                 </div>
                             </div>
-                       <h4>Brewers Tips:</h4>
-                        <p style={{fontStyle: 'italic'}}>"{beer.brewers_tips}"</p>
-                        <p style={{fontStyle: 'italic', color:'grey'}}><span>By</span> {beer.contributed_by} </p>
-    
+                            <h4>Brewers Tips:</h4>
+                            <p style={{fontStyle: 'italic'}}>"{beer.brewers_tips}"</p>
+                            <p style={{fontStyle: 'italic', color:'grey'}}><span>By</span> {beer.contributed_by} </p>
                         </CardContent>
                     </Card>
                 </article>
