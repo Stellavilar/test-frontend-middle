8e067e972483d1ce81618ee91372da0fc64d8b43
Stella Vilar <vilar.stella@gmail.com>
2021-05-17 12:33:40 +0200

    Making of middleware in order to save the cart into localStorage

---
+++ src/context/useLocalStorage.js
@@ -0,0 +1,22 @@
+//File created to save the cart into localStorage
+import { useState, useEffect } from 'react';
+
+//Function that is used to save a key and a value into the localStorage
+export default function useLocalStorage(key, initialValue) {
+    const prefixedKey = key;
+
+    const [ value, setValue ] = useState(() => {
+        const jsonValue = localStorage.getItem(prefixedKey);
+
+        if (jsonValue != null) return JSON.parse(jsonValue);
+
+        return initialValue;
+    });
+
+    useEffect(() => {
+        //Store key + stringifyValue into localStorage.
+        localStorage.setItem(prefixedKey, JSON.stringify(value))
+    }, [prefixedKey, value]);
+
+    return [ value, setValue];
+};
