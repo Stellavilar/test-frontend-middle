e098d3207da688b50c550aa1800e8a2cfa87214e
Stella Vilar <vilar.stella@gmail.com>
2021-05-17 11:10:05 +0200

    Making of Routes and components(List,Details,Products,Cart)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import '../styles/index.scss';
 
 import Header from './Header';
+import List from './List';
+import Details from './Details';
+import Cart from './Cart';
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
-    </>
+      <Route exact path="/">
+        <List />
+      </Route>
+      <Route exact path="/details/:id">
+        <Details />
+      </Route>
+      <Route exact path="/cart">
+        <Cart />
+      </Route>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import logo from '../img/logo.png';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
     return (
         <header>
-            <img src={logo} alt="logo" />
+            <Link to="/">
+                <img src={logo} alt="logo" />
+            </Link>
             <div className="search">
                 search input
             </div>
-            <div className="cart-icon">
-                <ShoppingCartIcon fontSize="large" />
-                Cart(0)
-            </div>
+            <Link to="/cart">
+                <div className="cart-icon">
+                    <ShoppingCartIcon fontSize="large" />
+                    Cart(0)
+                </div>
+            </Link>
         </header>
     );
 };
